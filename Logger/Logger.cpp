e341d24e179096693aed2bef840813f894f3beef
Said <saidmahov10@gmail.com>
2025-07-07 21:34:31 +0300

    Add AbstractLogger and SimpleLogger

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,7 +1,34 @@
 ﻿#include <print>
+#include <string>
+#include <string_view>
+
+class AbstractLogger {
+public:
+	virtual ~AbstractLogger() = default;
+	virtual void log(std::string_view msg) = 0;
+};
+
+class SimpleLogger : public AbstractLogger {
+private:
+	std::string name;
+public:
+	explicit SimpleLogger(std::string_view name = "") : name(name) {}
+	~SimpleLogger() = default;
+
+	void log(std::string_view msg) override {
+		if (!name.empty()) {
+			std::print("[{}] {}", name, msg);
+		}
+		else {
+			std::print("{}", msg);
+		}
+	}
+};
 
 int main() {
-    std::print("hello");
+	SimpleLogger logger = SimpleLogger("well");
 
-    return 0;
+	logger.log("hello");
+
+	return 0;
 }
